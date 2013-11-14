c42d993f491cbfd5da2eafa409046e86b861f086
lay595715148 <lay595715148@126.com>
2013-11-14 10:12:03 +0800

    remove bootstrap.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php 
 $st = date('Y-m-d H:i:s').'.'.floor(microtime()*1000);
-include_once __DIR__.'/bootstrap.php';
+include_once __DIR__.'/bootstrap.example.php';
 Laywork::start();
 Debugger::debug(array($st, date('Y-m-d H:i:s').'.'.floor(microtime() * 1000)));
 //echo '<pre>';print_r(get_included_files());echo '</pre>';
