b74134fc0c5c81c9e66a01e76f58828e10e6b855
lay595715148 <lay595715148@126.com>
2013-12-03 17:24:51 +0800

    move ob output to laybug

--- bootstrap.example.php
+++ bootstrap.example.php
@@ -5,12 +5,6 @@
  */
 define('INIT_LAYWORK', true);//标记
 
-ini_set('output_buffering', 'on');
-ini_set('implicit_flush', 'off');
-//Turn on output buffering
-//ob_start();
-ob_implicit_flush(false);
-
 require_once __DIR__.'/lib/PHP-Error/src/php_error.php';\php_error\reportErrors();
 require_once __DIR__.'/lib/index.php';
 require_once __DIR__.'/src/Laywork.php';
